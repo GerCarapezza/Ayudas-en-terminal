c6b6c8c2435a69c6eed3036990d0311baab29501
nicoScatolon <nscatolon@frba.utn.edu.ar>
2025-11-29 13:21:25 -0300

    vava

--- ayudita_querys.sh
+++ ayudita_querys.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-BIN="./home/utnso/tp-2025-2c-Here-we-go-again/query_control/bin/query_control"
+BIN="/home/utnso/tp-2025-2c-Here-we-go-again/query_control/bin/query_control"
 
 echo "Levantando 100 queries en 4 grupos..."
 
-# Grupo 1
 for i in $(seq 1 25); do
     $BIN DEFAULT AGING_1 20 &
 done
 echo "Grupo 1 levantado (25 procesos)"
 
-# Grupo 2
 for i in $(seq 1 25); do
     $BIN DEFAULT AGING_2 20 &
 done
 echo "Grupo 2 levantado (25 procesos)"
 
-# Grupo 3
 for i in $(seq 1 25); do
     $BIN DEFAULT AGING_3 20 &
 done
 echo "Grupo 3 levantado (25 procesos)"
 
-# Grupo 4
 for i in $(seq 1 25); do
     $BIN DEFAULT AGING_4 20 &
 done
